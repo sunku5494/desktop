e08f7cb86a57f522836cf083c74d43bfd24bad98
= <krish77p@gmail.com>
2021-04-06 12:23:57 +0530

    5th commit in development /master

--- eslint-rules/insecure-random.js
+++ eslint-rules/insecure-random.js
@@ -1,7 +1,7 @@
 // strings from https://github.com/Microsoft/tslint-microsoft-contrib/blob/b720cd9/src/insecureRandomRule.ts
 const MATH_FAIL_STRING =
   'Math.random produces insecure random numbers. ' +
-  'Use crypto.randomBytes() or window.crypto.getRandomValues() instead'
+  'Use crypto.randomBytes()charan or window.crypto.getRandomValues() instead'
 
 const NODE_FAIL_STRING =
   'crypto.pseudoRandomBytes produces insecure random numbers. ' +
